fe08097a787e09b8c14c985f027c03f5bd9f6f3b
Qluks <lucasmmedeirosm@gmail.com>
2022-06-21 13:00:30 -0300

    MyCard App

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,99 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Container(),
-      ),
-    ),
+    MeuApp(),
   );
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.yellow[800],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('imagens/avatar_1.jpeg'),
+                radius: 50,
+              ),
+              Text(
+                'Lucas Medeiros',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'DESENVOLVEDOR FLUTTER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.yellow[100],
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 250,
+                child: Divider(
+                  color: Colors.yellow[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.vpn_key,
+                    size: 25,
+                    color: Colors.yellow[800],
+                  ),
+                  title: Text(
+                    '+55 81 98712-0276',
+                    style: TextStyle(
+                      color: Colors.yellow[800],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    size: 25,
+                    color: Colors.yellow[800],
+                  ),
+                  title: Text(
+                    'lucasmmedeirosm@gmail.com',
+                    style: TextStyle(
+                      color: Colors.yellow[800],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
